69ddeeed17c8665a6b45488d0dfd1ab82b6b8f02
LeMernbag <62549129+LeMernbag@users.noreply.github.com>
2020-07-08 14:42:27 -0500

    Starter File Revamp
    Made into buttons instead of grid/frame, added popup and main window classes.

--- bd_main.py
+++ bd_main.py
@@ -1,71 +1,53 @@
-from tkinter import*
+from tkinter import *
+import sys
 
-master=Tk()
-master.geometry("800x300")
+class popupWindow(object):
+    def __init__(self,master):
+        top=self.top=Toplevel(master)
+        self.l=Label(top,text="To Do:")
+        self.l.pack()
+        self.e=Entry(top)
+        self.e.pack()
+        self.b=Button(top,text='Ok',command=self.cleanup)
+        self.b.pack()
+    def cleanup(self):
+        self.value=self.e.get()
+        self.top.destroy()
+        print(self.value)
 
-def button1(click):
-    label=Label(frame1, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
+class mainWindow(object):
+    def __init__(self,master):
+        self.master=master
+        self.master.title("Brain Dump")
+        self.button1=Button(master, width=20, height=10, command=self.popup)
+        self.button1.grid(row=0, column=0)
+        self.button2=Button(master, width=20, height=10, command=self.popup)
+        self.button2.grid(row=0, column=1)
+        self.button3=Button(master, width=20, height=10, command=self.popup)
+        self.button3.grid(row=0, column=2)
+        
+        self.button4=Button(master, width=20, height=10, command=self.popup)
+        self.button4.grid(row=1, column=0)
+        self.button5=Button(master, width=20, height=10, command=self.popup)
+        self.button5.grid(row=1, column=1)
+        self.button6=Button(master, width=20, height=10, command=self.popup)
+        self.button6.grid(row=1, column=2)
 
-def button2(click):
-    label=Label(frame2, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
+        self.button7=Button(master, width=20, height=10, command=self.popup)
+        self.button7.grid(row=2, column=0)
+        self.button8=Button(master, width=20, height=10, command=self.popup)
+        self.button8.grid(row=2, column=1)
+        self.button9=Button(master, width=20, height=10, command=self.popup)
+        self.button9.grid(row=2, column=2)
+        
+    def popup(self):
+        self.w=popupWindow(self.master)
+        self.master.wait_window(self.w.top)
 
-def button3(click):
-    label=Label(frame3, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
+    def entryValue(self):
+        return self.w.value
 
-def button4(click):
-    label=Label(frame4, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
-
-def button5(click):
-    label=Label(frame5, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
-
-def button6(click):
-    label=Label(frame6, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
-
-def button7(click):
-    label=Label(frame7, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
-
-def button8(click):
-    label=Label(frame8, text="Welcome", fg="cadet blue", height="9", width="22")
-    label.pack(side=TOP)
-
-frame1=Frame(master, width=200, height=150, background="cadet blue")
-frame1.grid(row=0, column=0)
-frame1.bind("<ButtonRelease>", button1)
-
-frame2=Frame(master, width=200, height=150, background="light cyan")
-frame2.grid(row=0, column=1)
-frame2.bind("<ButtonRelease>", button2)
-
-frame3=Frame(master, width=200, height=150, background="cadet blue")
-frame3.grid(row=0, column=2)
-frame3.bind("<ButtonRelease>", button3)
-
-frame4=Frame(master, width=200, height=150, background="light cyan")
-frame4.grid(row=0, column=3)
-frame4.bind("<ButtonRelease>", button4)
-
-
-frame5=Frame(master, width=200, height=150, background="light cyan")
-frame5.grid(row=1, column=0)
-frame5.bind("<ButtonRelease>", button5)
-
-frame6=Frame(master, width=200, height=150, background="cadet blue")
-frame6.grid(row=1, column=1)
-frame6.bind("<ButtonRelease>", button6)
-
-frame7=Frame(master, width=200, height=150, background="light cyan")
-frame7.grid(row=1, column=2)
-frame7.bind("<ButtonRelease>", button7)
-
-frame8=Frame(master, width=200, height=150, background="cadet blue")
-frame8.grid(row=1, column=3)
-frame8.bind("<ButtonRelease>", button8)
-
-master.mainloop()
+if __name__ == "__main__":
+    root=Tk()
+    m=mainWindow(root)
+    root.mainloop()
